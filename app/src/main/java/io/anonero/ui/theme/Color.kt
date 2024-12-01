69507961bd9a90f6fe5b9bb3dc72a57077f75543
anonym <0xanonym@proton.me>
2024-12-01 20:41:55 +0100

    update theme with robotomono as default

--- app/src/main/java/io/anonero/ui/theme/Color.kt
+++ app/src/main/java/io/anonero/ui/theme/Color.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.graphics.Color
 val Orange = Color(0xFFFF6601)
 val DarkOrange = Color(0xFFCC5200)
 val Black = Color(0xFF000000)
-val DarkGray = Color(0xFF121212)
+val DarkGray = Color(0xFF09090b)
 val Gray = Color(0xFF333333)
 val White = Color(0xFFFFFFFF)
 
--- app/src/main/java/io/anonero/ui/theme/Theme.kt
+++ app/src/main/java/io/anonero/ui/theme/Theme.kt
@@ -1,6 +1,10 @@
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import io.anonero.R
 import io.anonero.ui.theme.*
 
 private val DarkColorScheme = darkColorScheme(
@@ -8,7 +12,7 @@ private val DarkColorScheme = darkColorScheme(
     onPrimary = Black,
     primaryContainer = DarkOrange,
     onPrimaryContainer = White,
-    secondary = Black,
+    secondary = DarkGray,
     onSecondary = White,
     background = Black,
     onBackground = White,
@@ -16,13 +20,38 @@ private val DarkColorScheme = darkColorScheme(
     onSurface = White
 )
 
+val robotoMonoFamily = FontFamily(
+    Font(R.font.robotomono_light, FontWeight.Light),
+    Font(R.font.robotomono_regular, FontWeight.Normal),
+    Font(R.font.robotomono_medium, FontWeight.Medium),
+    Font(R.font.robotomono_bold, FontWeight.Bold),
+    Font(R.font.robotomono_semi_bold, FontWeight.SemiBold),
+)
+
+
 @Composable
 fun AnonNeroTheme(
     content: @Composable () -> Unit
 ) {
-
     MaterialTheme(
         colorScheme = DarkColorScheme,
-        content = content
+        content = content,
+        typography = Typography.copy(
+            titleLarge = Typography.titleLarge.copy(fontFamily = robotoMonoFamily),
+            titleSmall = Typography.titleSmall.copy(fontFamily = robotoMonoFamily),
+            titleMedium = Typography.titleMedium.copy(fontFamily = robotoMonoFamily),
+            headlineMedium = Typography.headlineMedium.copy(fontFamily = robotoMonoFamily),
+            headlineLarge = Typography.headlineLarge.copy(fontFamily = robotoMonoFamily),
+            headlineSmall = Typography.headlineSmall.copy(fontFamily = robotoMonoFamily),
+            bodyLarge = Typography.bodyLarge.copy(fontFamily = robotoMonoFamily),
+            bodySmall = Typography.bodySmall.copy(fontFamily = robotoMonoFamily),
+            bodyMedium = Typography.bodyMedium.copy(fontFamily = robotoMonoFamily),
+            displayLarge = Typography.displayLarge.copy(fontFamily = robotoMonoFamily),
+            displaySmall = Typography.displaySmall.copy(fontFamily = robotoMonoFamily),
+            displayMedium = Typography.displayMedium.copy(fontFamily = robotoMonoFamily),
+            labelLarge = Typography.labelLarge.copy(fontFamily = robotoMonoFamily),
+            labelSmall = Typography.labelSmall.copy(fontFamily = robotoMonoFamily),
+            labelMedium = Typography.labelMedium.copy(fontFamily = robotoMonoFamily),
+        )
     )
 }
