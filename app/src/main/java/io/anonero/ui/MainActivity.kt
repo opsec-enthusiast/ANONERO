3c5cc407cd5eeeb154f8589c498e09c49ad8015b
anonym <0xanonym@proton.me>
2025-03-15 20:56:31 +0100

    show tor splash only if custom proxy is not set

--- app/src/main/java/io/anonero/ui/MainActivity.kt
+++ app/src/main/java/io/anonero/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package io.anonero.ui
 import AnonNeroTheme
 import android.Manifest
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -14,38 +15,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.asLiveData
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import io.anonero.AnonConfig
-import io.anonero.R
 import io.anonero.model.WalletManager
 import io.anonero.services.AnonNeroService
 import io.anonero.services.TorService
@@ -56,13 +39,15 @@ import io.anonero.ui.home.graph.routes.Home
 import io.anonero.ui.onboard.OnboardViewModel
 import io.anonero.ui.onboard.graph.LandingScreenRoute
 import io.anonero.ui.onboard.graph.onboardingGraph
+import io.anonero.util.WALLET_PREFERENCES
+import io.anonero.util.WALLET_USE_TOR
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import org.koin.androidx.compose.koinViewModel
+import org.koin.core.qualifier.named
 import timber.log.Timber
 
 
@@ -73,98 +58,45 @@ class MainActivity : ComponentActivity() {
     private val scope: CoroutineScope = CoroutineScope(Dispatchers.IO)
     private val walletState: WalletState by inject()
     private val torService: TorService by inject()
-
+    val anonPrefs: SharedPreferences by inject(named(WALLET_PREFERENCES))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashscreen = installSplashScreen()
         var isAppReady by mutableStateOf(false)
         super.onCreate(savedInstanceState)
-
-        splashscreen.setKeepOnScreenCondition { false }
-
         if (intent.hasExtra("notification")) {
             walletState.setBackGroundSync(false)
             walletState.update()
         }
 
         val scrimColor = Color.Black.copy(alpha = 0.1f).toArgb()
-
+        splashscreen.setKeepOnScreenCondition { isAppReady }
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.dark(scrimColor),
             navigationBarStyle = SystemBarStyle.dark(scrimColor),
         )
         setContent {
             var walletExist by remember { mutableStateOf(AnonConfig.isWalletFileExist()) }
-            val scope = rememberCoroutineScope()
+            var useTor by remember { mutableStateOf(true) }
             val onboardViewModel = koinViewModel<OnboardViewModel>()
-            val socks by torService.socksFlow.asLiveData().observeAsState(null)
+            val context = LocalContext.current
+            val scope = rememberCoroutineScope()
 
-            val progressAnim by animateFloatAsState(
-                targetValue = if (socks != null) .98f else 0.4f,
-                label = "progressAnim"
-            )
-
-            LaunchedEffect(key1 = walletExist, socks) {
-                if (socks != null) {
-                    scope.launch(Dispatchers.IO) {
-                        walletExist = AnonConfig.getDefaultWalletFile(applicationContext).exists()
-                        delay(200)
-                        isAppReady = true
-                    }
+            LaunchedEffect(key1 = true) {
+                scope.launch(Dispatchers.IO) {
+                    walletExist = AnonConfig.getDefaultWalletFile(context).exists()
+                    useTor = anonPrefs.getBoolean(WALLET_USE_TOR, true)
+                    isAppReady = true
                 }
             }
-            if (!isAppReady) {
-                return@setContent Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_anon),
-                        contentDescription = "Logo",
-                        modifier = Modifier
-                            .size(120.dp)
-                    )
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(16.dp),
-                        verticalArrangement = Arrangement.Bottom,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        LinearProgressIndicator(
-                            progress = { progressAnim },
-                            modifier = Modifier
-                                .fillMaxWidth(0.6f)
-                                .padding(
-                                    bottom = 24.dp
-                                ),
-                            strokeCap = StrokeCap.Round,
-                            color = Color.White,
-                            drawStopIndicator = {},
-                            trackColor = Color.White.copy(alpha = 0.2f)
-                        )
-                        Text(
-                            text = "Waiting for Tor ...",
-                            color = Color.White
-                        )
-                    }
-                }
-
-            } else {
+            TorSplash(enableTor = useTor) {
                 AnonNeroTheme {
                     val navController = rememberNavController()
-                    NavHost(
-                        navController = navController,
-                        popExitTransition = {
-                            ExitTransition.None
-                        },
-                        popEnterTransition = {
-                            EnterTransition.None
-                        },
-                        startDestination = if (walletExist) Home else LandingScreenRoute
+                    NavHost(navController = navController, popExitTransition = {
+                        ExitTransition.None
+                    }, popEnterTransition = {
+                        EnterTransition.None
+                    }, startDestination = if (walletExist) Home else LandingScreenRoute
                     ) {
                         //landing screen and onboarding screens
                         onboardingGraph(navController, onboardViewModel)
@@ -181,11 +113,10 @@ class MainActivity : ComponentActivity() {
         ActivityResultContracts.RequestPermission()
     ) { isGranted ->
         if (isGranted) {
-            Intent(applicationContext, AnonNeroService::class.java)
-                .also {
-                    it.action = "start"
-                    ContextCompat.startForegroundService(applicationContext, it)
-                }
+            Intent(applicationContext, AnonNeroService::class.java).also {
+                it.action = "start"
+                ContextCompat.startForegroundService(applicationContext, it)
+            }
         }
         //handle and show dialog
     }
@@ -193,8 +124,7 @@ class MainActivity : ComponentActivity() {
     fun startNotificationService() {
         // Check if the permission is already granted
         if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.POST_NOTIFICATIONS
+                this, Manifest.permission.POST_NOTIFICATIONS
             ) != PackageManager.PERMISSION_GRANTED
         ) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
@@ -206,11 +136,10 @@ class MainActivity : ComponentActivity() {
     }
 
     fun stopNotificationService() {
-        Intent(applicationContext, AnonNeroService::class.java)
-            .also {
-                it.action = "stop"
-                ContextCompat.startForegroundService(applicationContext, it)
-            }
+        Intent(applicationContext, AnonNeroService::class.java).also {
+            it.action = "stop"
+            ContextCompat.startForegroundService(applicationContext, it)
+        }
     }
 
     override fun onDestroy() {
@@ -230,4 +159,5 @@ class MainActivity : ComponentActivity() {
         Process.killProcess(Process.myPid())
     }
 }
+
 
---
+++ app/src/main/java/io/anonero/ui/TorSplash.kt
@@ -0,0 +1,120 @@
+package io.anonero.ui
+
+import android.content.SharedPreferences
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.asLiveData
+import io.anonero.AnonConfig
+import io.anonero.R
+import io.anonero.services.TorService
+import io.anonero.util.WALLET_PREFERENCES
+import io.anonero.util.WALLET_USE_TOR
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.koin.compose.koinInject
+import org.koin.core.qualifier.named
+
+@Composable
+fun TorSplash(
+    modifier: Modifier = Modifier,
+    enableTor: Boolean,
+    content: @Composable () -> Unit = {}
+) {
+    var walletExist by remember { mutableStateOf(AnonConfig.isWalletFileExist()) }
+    var anonPrefs: SharedPreferences = koinInject<SharedPreferences>(named(WALLET_PREFERENCES))
+    val torService: TorService = koinInject<TorService>()
+    val socks by torService.socksFlow.asLiveData().observeAsState(null)
+    val scope = rememberCoroutineScope()
+    var isAppReady by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+
+    val progressAnim by animateFloatAsState(
+        targetValue = if (socks != null) .98f else 0.4f, label = "progressAnim"
+    )
+
+    LaunchedEffect(key1 = walletExist, socks) {
+        scope.launch(Dispatchers.IO) {
+            walletExist = AnonConfig.getDefaultWalletFile(context).exists()
+            //if user disabled tor and uses manual proxy, skip tor splash
+            if (!anonPrefs.getBoolean(WALLET_USE_TOR, true)) {
+                isAppReady = true
+            } else {
+                if ((socks != null)) scope.launch(Dispatchers.IO) {
+                    delay(200)
+                    isAppReady = true
+                }
+            }
+        }
+    }
+
+    //no need to show tor splash if user disabled tor
+    if (!enableTor) {
+        return content();
+    }
+    AnimatedContent(targetState = isAppReady) {
+        if (it) {
+            content()
+        } else {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.ic_anon),
+                    contentDescription = "Logo",
+                    modifier = Modifier.size(120.dp)
+                )
+                Column(
+                    modifier = Modifier.padding(
+                        horizontal = 16.dp, vertical = 16.dp
+                    ),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    LinearProgressIndicator(
+                        progress = { progressAnim },
+                        modifier = Modifier
+                            .fillMaxWidth(0.6f)
+                            .padding(
+                                horizontal = 16.dp, vertical = 16.dp
+                            ),
+                        strokeCap = StrokeCap.Round,
+                        color = Color.White,
+                        drawStopIndicator = {},
+                        trackColor = Color.White.copy(alpha = 0.2f)
+                    )
+                    Text(
+                        text = "Waiting for Tor ...", color = Color.White
+                    )
+                }
+            }
+        }
+    }
+}
