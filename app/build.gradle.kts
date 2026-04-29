cfa22234aeb3db5b4dcbb69f0ff8fa1556d0b7d3
ANONERO <anonero@proton.me>
2026-04-29 10:13:42 -0400

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 3
-        versionName = "1.0"
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
